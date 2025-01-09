7eb77848e99964e655152041bf6cff9bb62277f7
Sharath <sharathck@outlook.com>
2025-01-09 00:58:53 -0600

    Deploying to Firebase

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,10 @@ firebase deploy
 echo "Deployment complete!"
 
 # Run the script, git add, commit and push the changes
+echo "Running the script to add stage all changes..."
 git add --all
+echo "Running the script to commit changes..."
 git commit -m "Deploying to Firebase"
+echo "Running the script to push changes..."
 git push -u origin main
+echo "All done!"
